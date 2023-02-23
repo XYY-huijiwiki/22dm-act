9eceadf9d33d60c4d1bd1477d80a5d75ff2343c5
Karsten Zhou <1339688323@qq.com>
2023-02-24 00:07:26 +0800

    0

--- readme.md
+++ readme.md
@@ -1,21 +1,46 @@
-# 有爱成双（bus）
- 
-原创动力的活动网址“有爱成双”的原版拷贝及修改版本。
+# 原创动力活动网站存档
 
-## 原始版本
-文件夹`www.22dm.com`内存储着游戏的原始版本。因为游戏有排行系统，需要直接向微信获取信息，所以必须在微信中打开才行。现在打开游戏会提示`Anmeldung bei Weixin nicht möglich - This Official Account does not have these "scope" permissions. Error code:10005`，无法正常游玩。
+## 有爱成双（bus）
+原创动力的活动网站“有爱成双”的原版拷贝及修改版本。
 
-## 修改版本
-文件夹`xyy-huijiwiki.github.io`内存储着游戏的修改版本。修改内容如下：
-- 去除了所有微信和百度相关的代码，确保游戏能够独立运行。
+### 原始版本
+文件夹`www.22dm.com`内存储着活动网站的原始版本。活动网站在微信中打开。现在打开活动网站会报错：`Anmeldung bei Weixin nicht möglich - This Official Account does not have these "scope" permissions. Error code:10005`。
+
+### 修改版本
+文件夹`xyy-huijiwiki.github.io`内存储着活动的修改版本。修改内容如下：
+- 去除了所有微信和百度相关的代码，确保活动网站能正常打开。
 - 领奖时不会真的向服务器发送消息。
 - 制作了两个版本，分别对应是否获奖的两种情况。
-- 排行榜内嵌至游戏代码中——反正活动结束后排行榜就固定了，没必要总是向服务器发送请求。
+- 排行榜内嵌至活动代码中——反正活动结束后排行榜就固定了，没必要总是向服务器发送请求。
 - 表白消息也嵌入了，因为无法跨域请求数据。其中删除了一条消息，因为报错了。
 ![](Screenshot%202023-02-23%20154822.jpg)
 
-## 链接
-- [官方游戏链接](http://www.22dm.com/act/h5/bus)
-- [修改版游戏链接1](https://xyy-huijiwiki.github.io/bus)（未获奖）
-- [修改版游戏链接2](https://xyy-huijiwiki.github.io/bus/xyy-huijiwiki.github.io/act/h5/bus/index2.html)（获奖）
+### 链接
+- [官方活动链接](http://www.22dm.com/act/h5/bus)
+- [修改版活动链接1](https://xyy-huijiwiki.github.io/22dm-act/xyy-huijiwiki.github.io/act/h5/bus/index.html)（未获奖）
+- [修改版活动链接2](https://xyy-huijiwiki.github.io/22dm-act/xyy-huijiwiki.github.io/act/h5/bus/index2.html)（获奖）
 - [羊羊百科](https://xyy.huijiwiki.com/wiki/有爱成双)
+
+
+## “募”名而来（collect）
+原创动力的活动网站“‘募’名而来”的原版拷贝及修改版本。
+
+### 原始版本
+文件夹`www.22dm.com`内存储着活动网站的原始版本。活动网站在微信中打开。现在打开活动网站会报错：`Anmeldung bei Weixin nicht möglich - This Official Account does not have these "scope" permissions. Error code:10005`。
+
+### 修改版本
+文件夹`xyy-huijiwiki.github.io`内存储着活动的修改版本。修改内容如下：
+- 去除了所有微信和百度相关的代码，确保活动网站能正常打开。
+- 领奖时不会真的向服务器发送消息。
+- 制作了三个版本，分别对应各种获奖情况。
+- 因为无法获知领奖成功后服务器会返回什么信息，根据修改[有爱成双](#有爱成双bus)的经验，我假设返回的信息为`信息提交成功!奖品于7个工作日内发出`。
+
+### 链接
+- [官方活动链接](http://www.22dm.com/act/h5/collect)
+- [修改版活动链接1](https://xyy-huijiwiki.github.io/22dm-act/xyy-huijiwiki.github.io/act/h5/collect/index.html)（未获奖）
+- [修改版活动链接2](https://xyy-huijiwiki.github.io/22dm-act/xyy-huijiwiki.github.io/act/h5/collect/index2.html)（获一二三等奖）
+- [修改版活动链接3](https://xyy-huijiwiki.github.io/22dm-act/xyy-huijiwiki.github.io/act/h5/collect/index3.html)（获参与奖）
+- [羊羊百科](https://xyy.huijiwiki.com/wiki/“募”名而来)
+
+### 秘密
+活动结束后html和js文件的代码就被修改了，只显示活动结束后的页面。好消息是，css文件没被修改；更好的消息是，图片地址就是写在css文件里的；极好的消息是，图片一张也没被删除。你可以[点击这里](./www.22dm.com/act/h5/collect/images/)查看全部相关图片，或者打开修改版活动链接后，按F12在浏览器控制台中输入`$("#collect").fadeIn()`查看这个页面。点击提交按钮会报错——对此我无能为力，因为相关代码已经被删得一干二净。
---
+++ xyy-huijiwiki.github.io/act/h5/collect/js/index2.js
@@ -0,0 +1,235 @@
+var token, openid, debug = true;
+var weixinData = {
+	loginUrl: 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wxb61fdb0e387cbcc4&redirect_uri=http%3a%2f%2fwww.22dm.com%2fact%2fh5%2fcollect%2findex.html&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect',
+	shareTitle: '嘻哈闯世界3，“募”名而来',
+	shareDesc: '一起嘻嘻哈哈闯世界！《嘻哈闯世界3》的名字由你说了算！现在参与赢羊村大奖！',
+	shareLink: 'http://www.22dm.com/act/h5/collect/index.html',
+	shareImg: 'http://www.22dm.com/act/h5/collect/images/icon.jpg'
+};
+var userInfo = {
+	openid: "test_act_id_26",
+	name: "test",
+	sex: "",
+	country: "",
+	province: "",
+	timespan: 0,
+	json: { "info": {}, "collect": [] },   //我填写过的信息
+	hasPrize: false,
+	award: false,
+	prize: 0
+};
+$(function () {
+	var width = $(window).width();
+	var height = $(window).height();
+	if (width > 640)
+		$("#all").css("left", (width - 640) >> 1);
+	if (height > 1008)
+		$("#all").css("top", (height - 1008) >> 1);
+	if (getQueryString("code") == null) {
+		init();
+		// location.href = weixinData.loginUrl;
+	}
+	else {
+		$.ajax({
+			cache: false,
+			async: false,
+			type: "POST",
+			url: '/act/center/openIdCrossDomain.ashx',
+			data: {
+				"type": "GET",
+				"url": 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxb61fdb0e387cbcc4&secret=2de4747ccccb04312e71e86708bfec66&code=' + getQueryString("code") + '&grant_type=authorization_code'
+			},
+			success: function (response) {
+				var result = JSON.parse(response);
+				token = result.access_token;
+				openid = result.openid;
+				if (typeof (openid) == "undefined") {
+					location.href = weixinData.loginUrl;
+				}
+				else {
+					$.ajax({
+						cache: false,
+						async: false,
+						type: "POST",
+						url: '/act/center/openIdCrossDomain.ashx',
+						data: {
+							"type": "GET",
+							"url": 'https://api.weixin.qq.com/sns/userinfo?access_token=' + token + '&openid=' + openid + '&lang=zh_CN',
+						},
+						success: function (res) {
+							var r = JSON.parse(res);
+							userInfo.openid = openid;
+							console.info(userInfo.openid);
+							init();
+						}
+					});
+				}
+			}
+		});
+
+	}
+});
+
+function init() {
+	$(".layer_open").click(function () {
+		var tar = '#' + $(this).attr("tar");
+		$(tar).fadeIn();
+	});
+	$(".layer_close").click(function () {
+		$(this).parent().hide();
+	});
+	$("#dialogs_close").click(function () {
+		$("#dialogs").fadeOut();
+	});
+	$("#award_check,#award_check_top").click(function () {
+		if (userInfo.hasPrize) {
+			if (!userInfo.award) {
+				if (userInfo.prize == 1) {
+					$("#award").fadeIn();
+				}
+				else if (userInfo.prize == 11) {
+					$("#dialogs_tips").empty().html('恭喜你获得了50元天猫优惠券<br/><a href="https://taoquan.taobao.com/coupon/unify_apply.htm?sellerId=2473210771&activityId=99e199c1725c4795a6e8aaa7d7d4d72f">点击此处前往领取</a>').parent().parent().fadeIn();
+				}
+			}
+			else {
+				$("#dialogs_tips").empty().html('<br/>你已经填写过获奖信息！').parent().parent().fadeIn();
+			}
+		}
+		else {
+			$("#dialogs_tips").empty().html('<br/>本次活动你没有上榜！').parent().parent().fadeIn();
+		}
+	});
+	// $.ajax({
+	// 	cache: false,
+	// 	async: false,
+	// 	type: "POST",
+	// 	url: 'collect.ashx',
+	// 	data: {
+	// 		type: "ranking",
+	// 		user: userInfo.openid
+	// 	},
+	// 	success: function (res) {
+	// 		console.info(res);
+	// 		var data = JSON.parse(res);
+	// 		userInfo.hasPrize = data.hasPrize;
+	// 		userInfo.prize = data.prize;
+	// 		userInfo.award = data.award;
+	// 		initWxJsSdk();
+	// 	}
+	// });
+	let data = {
+		"hasPrize": true,
+		"prize": 1,
+		"award": false
+	};
+	userInfo.hasPrize = data.hasPrize;
+	userInfo.prize = data.prize;
+	userInfo.award = data.award;
+	// initWxJsSdk();
+}
+var award = {
+	check: function () {
+		$("#award_tips").empty();
+		var name = $("#award_name").val();
+		var tel = $("#award_tel").val();
+		var address = $("#award_address").val().replace(/\s/g, "");
+		var result = "";
+		if (name == "") {
+			result = "请填写收件人姓名！";
+		}
+		else if (tel.length != 11) {
+			result = "请填写11位收件人手机号码！";
+		}
+		else if (address == "" || address.length < 8) {
+			result = "请填写详细的收件人地址！";
+		}
+		if (result == "") { //都正确
+			var date = new Date();
+			var addtime = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+			var info = '{"name":"' + name + '","tel":"' + tel + '","address":"' + address + '","addtime":"' + addtime + '","prize":' + userInfo.prize + '}';
+			award.post(info);
+		}
+		else {
+			$("#dialogs_tips").empty().html('<br/>' + result).parent().parent().fadeIn();
+			return false;
+		}
+	},
+	post: function (info) {
+		// $.ajax({
+		// 	cache: false,
+		// 	async: false,
+		// 	type: "POST",
+		// 	url: 'collect.ashx',
+		// 	data: {
+		// 		"type": "award",
+		// 		"user": userInfo.openid,
+		// 		"json": info
+		// 	},
+		// 	success: function (response) {
+		// 		console.info(response);
+		// 		var result = JSON.parse(response);
+		// 		$("#dialogs_tips").empty().html(result.returnStr).parent().parent().fadeIn();
+		// 		if (result.success)
+		// 			userInfo.award = true;
+		// 	}
+		// });
+		$("#dialogs_tips").empty().html('信息提交成功!奖品于7个工作日内发出。').parent().parent().fadeIn();
+	}
+};
+function initWxJsSdk() {
+	$.ajax({
+		cache: false,
+		async: false,
+		type: "POST",
+		url: '/act/getWeiXinToken.ashx',
+		data: {
+			thisUrl: (location.href.split('#')[0])
+		},
+		success: function (res) {
+			var wxData = JSON.parse(res);
+			wx.config({
+				debug: false,
+				appId: wxData.appId,
+				timestamp: wxData.timestamp,
+				nonceStr: wxData.nonceStr,
+				signature: wxData.signature,
+				jsApiList: [
+					'onMenuShareTimeline',
+					'onMenuShareAppMessage'
+				]
+			});
+		}
+	});
+	wx.ready(function () {
+		wx.onMenuShareTimeline({
+			title: weixinData.shareTitle,
+			link: weixinData.shareLink,
+			imgUrl: weixinData.shareImg,
+			success: function () {
+				console.info("onMenuShareTimeline ok");
+
+			},
+			cancel: function () {
+				console.info("onMenuShareTimeline cancel");
+			}
+		});
+		wx.onMenuShareAppMessage({
+			title: weixinData.shareTitle,
+			desc: weixinData.shareDesc,
+			link: weixinData.shareLink,
+			imgUrl: weixinData.shareImg,
+			success: function () {
+				console.info("onMenuShareTimeline ok");
+			},
+			cancel: function () {
+				console.info("onMenuShareAppMessage cancel");
+			}
+		});
+	});
+}
+
+function getQueryString(name) {
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if (r != null) return (r[2]); return null;
+}
